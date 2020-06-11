9828e530d4a1f781a404ce2e2fd2eb6ffcbddd4a
Victoria Bezrukova <divavictoria95@gmail.com>
2020-06-11 15:52:39 +0300

    Chapter 6.1

---
+++ src/main/java/com/example/tacocloud/controller/DesignTacoRestController.java
@@ -0,0 +1,49 @@
+package com.example.tacocloud.controller;
+
+import com.example.tacocloud.Taco;
+import com.example.tacocloud.data.TacoRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoRestController {
+    private TacoRepository tacoRepository;
+
+    public  DesignTacoRestController(TacoRepository tacoRepository) {
+        this.tacoRepository = tacoRepository;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(0,12, Sort.by("createdAt").descending());
+        return tacoRepository.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optionalTaco = tacoRepository.findById(id);
+        return optionalTaco.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
+    }
+}
+
--- src/main/java/com/example/tacocloud/data/TacoRepository.java
+++ src/main/java/com/example/tacocloud/data/TacoRepository.java
@@ -1,7 +1,11 @@
 package com.example.tacocloud.data;
 
 import com.example.tacocloud.Taco;
+import java.net.ContentHandler;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
 }
